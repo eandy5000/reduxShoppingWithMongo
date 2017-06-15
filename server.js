6772a89e4f32e0c05af60a2677642ffd6a41431e
Andy Eccleston <eandy5000@yahoo.com>
2017-06-15 10:21:28 -0500

    added gitignore file

---
+++ server.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.set('port', (process.env.PORT || 4000))
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
+app.listen(app.get('port'), () => {
+    console.log(`Listening on port: ${app.get('port')}`)
+})
