8eca715d7c4cac6bf2b4a8c8b902f181822c1289
Andy Eccleston <eandy5000@yahoo.com>
2017-06-15 10:38:13 -0500

    added very basic redux for counter

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
-console.log('wrk')
+import {createStore} from 'redux'
+
+// step 3 define reducer
+const reducer = function (state = 0, action) {
+    switch (action.type) {
+        case 'INC':
+        return state + action.payload;
+        case 'DEC':
+        return state--
+    }
+    return state
+}
+
+// step 1 Create Store
+const store = createStore(reducer)
+
+// look at store state
+store.subscribe(function () {
+    console.log(`Store state: ${store.getState()}`)
+})
+
+// step 2 create and dispatch actions
+
+store.dispatch({type: 'INC', payload: 1})
+store.dispatch({type: 'INC', payload: 1})
+store.dispatch({type: 'DEC', payload: 1})
