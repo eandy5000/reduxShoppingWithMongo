574a00e7ab21e20fcfe4ae265d0a985f28e0a5c6
Andy Eccleston <eandy5000@yahoo.com>
2017-06-21 08:27:21 -0500

    reset project for rebuild

--- src/app.js
+++ src/app.js
@@ -1,31 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
 import {createStore} from 'redux'
-import {Provider} from 'react-redux'
-import reducer from './reducers/index.js'
-//actions
-import {inc, dec} from './actions/countActions'
-import {getBooks, addBook, updateTitle, deleteBook} from './actions/booksActions'
+
+function reducer (state=0, action) {
+    switch(action.type) {
+        default:
+        return state
+    }
+}
 
 const store = createStore(reducer)
 
-// dom and components
-const root = document.querySelector('#app')
-import BookList from './components/BookList'
-
-store.subscribe(function () {
-    console.log('store: ', store.getState().books)
+store.subscribe(() => {
+    console.log('Store: ', store.getState())
 })
 
-store.dispatch(getBooks())
-
-ReactDOM.render(
-    <Provider store={store}>
-        <BookList />
-    </Provider>
-    ,
-    root
-)
-
-
-
+store.dispatch({
+    type: 'TEST'
+})
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux'
-import {counterReducer} from './countReducer.js'
-import {booksReducer} from './booksReducer.js'
-
-export default combineReducers({
-    count: counterReducer,
-    books: booksReducer
-})
